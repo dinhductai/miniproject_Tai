1a8aecfb864b49a5b728db1c41de4297b6574b15
dinhductai <dinhductai2501@gmail.com>
2025-08-22 16:51:48 +0700

    chore: config discovery server

--- discovery-server/discovery-server/src/main/java/com/microsv/discovery_server/DiscoveryServerApplication.java
+++ discovery-server/discovery-server/src/main/java/com/microsv/discovery_server/DiscoveryServerApplication.java
@@ -2,7 +2,9 @@ package com.microsv.discovery_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer //Kích hoạt Eureka Serve
 @SpringBootApplication
 public class DiscoveryServerApplication {
 
